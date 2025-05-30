c1374d17bc936e7de39838b1cdc97efa5b614079
dian mushkov <dian.mushkov@gmail.com>
2025-05-30 10:13:52 +0300

    Add support for angular app

--- backend/src/main/java/com/example/employees/config/WebConfig.java
+++ backend/src/main/java/com/example/employees/config/WebConfig.java
@@ -4,18 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final String frontendUrl;
+    private final List<String> allowedOrigins;
 
-    public WebConfig(@Value("${frontend.url}") String frontendUrl) {
-        this.frontendUrl = frontendUrl;
+    public WebConfig(@Value("${frontend.urls}") String frontendUrls) {
+        this.allowedOrigins = Arrays.asList(frontendUrls.split(","));
     }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // or "/**" to allow everything
-                .allowedOrigins(frontendUrl) // Vite dev server
+                .allowedOrigins(allowedOrigins.toArray(new String[0])) // Vite dev server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
